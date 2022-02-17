3c7c7c247910eb9a54191c39175e72b878c09966
Erick Rosa <erickpmotta@gmail.com>
2022-02-17 17:58:28 -0300

    trazendo footer, nav, hero, limpando app, lipando conflitos de CSS

---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import '../css/hero.css'
+import '../css/nav-footer.css'
+
+export default function Hero() {
+  return (
+      <header className='hero-header'>
+          <div className="hero-container">
+
+                <h1>Encontre hashtags de maneira fácil.</h1>      
+                
+                <p>Digite o que deseja no campo de buscas e confira os resultados do Twitter abaixo</p>
+          </div>
+
+
+          <div className="hero-search-bar">
+
+                <input type="search" />
+
+          </div>
+
+
+    </header>
+  )
+}
