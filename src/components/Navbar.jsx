0db6072cbf6bd51fde13222b9277cc75aa491dbf
Erick Rosa <erickpmotta@gmail.com>
2022-02-18 14:51:06 -0300

    trabalhando testes no props do butao

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,53 +4,59 @@ import infoIcon from "../assets/img/icon-info-circle.svg";
 import logIcon from "../assets/img/icon-user-alt.svg";
 import { Link } from "react-router-dom";
 
-export default function Navbar({ aboutPage, logPage, homePage, buttonTitle, buttonIcon }) {
+export default function Navbar({
+  aboutPage,
+  logPage,
+  homePage,
+  buttonTitle,
+  buttonIcon,
+}) {
   return (
     <>
       <div className="navWrapper">
-      <nav className="navHeader">
-        <div className="navTitle">
-          <Link to="/">
-            {" "}
-            <h2>
-              hashtag<span>finder</span>
-            </h2>
-          </Link>
-         </div>
-    <div className="navMenu">
-          
-            {aboutPage && (<Link to={aboutPage}>
-
-      <button className="aboutBtn">
-            {" "}
-            <img src={infoIcon} alt="info-icon" /> Sobre
-          </button>
-
-            </Link>)}
-            
-
-            {logPage && (<Link to={logPage}>
-            <button className="loginBtn">
+        <nav className="navHeader">
+          <div className="navTitle">
+            <Link to="/">
               {" "}
-              <img src={logIcon} alt="icon-user" />
-              {buttonTitle}
-            </button>
-            </Link>)}
-            
+              <h2>
+                hashtag<span>finder</span>
+              </h2>
+            </Link>
+          </div>
+          <div className="navMenu">
+            {aboutPage && (
+              <Link to={aboutPage}>
+                <button className="aboutBtn">
+                  {" "}
+                  <img src={infoIcon} alt="info-icon" /> Sobre
+                </button>
+              </Link>
+            )}
 
-            {homePage && (<Link to={homePage}>
-      <button className="aboutBtn">
-            {" "}
-            <img src={buttonIcon} alt="info-icon" />Home
-          </button>
+                      {homePage && (
+              <Link to={homePage}>
+                <button className="aboutBtn">
+                  {" "}
+                  <img src={buttonIcon} alt="info-icon" />
+                  Home
+                </button>
+              </Link>
+            )}
 
-            </Link>)}
-       
-       
-     
-        </div>
+            {logPage && (
+              <Link to={logPage}>
+                <button className="loginBtn">
+                  {" "}
+                  <img src={buttonIcon} alt="icon-user" />
+                  {buttonTitle}
+                </button>
+              </Link>
+            )}
+
+  
+          </div>
         </nav>
-        </div>
+      </div>
     </>
   );
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,26 +1,20 @@
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import Hero from "../../components/Hero";
-import React from 'react';
-import "../../css/Home.css"
-
-
+import React from "react";
+import "../../css/Home.css";
 
 const Home = () => {
-    return (
-        <>
-            <Navbar logPage={"/login"} buttonTitle="Login" aboutPage={"/about"} />
-            <Hero />
-            <main className="main-home">
-                
-                <h2>Exibindo os 10 resultados mais recentes para #Natureza</h2>
-
-            </main>
-            <Footer />
-        
-
-        </>
-    );
-}
+  return (
+    <>
+      <Navbar logPage={"/login"} buttonTitle="Login" aboutPage={"/about"} />
+      <Hero />
+      <main className="main-home">
+        <h2>Exibindo os 10 resultados mais recentes para #Natureza</h2>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
 export default Home;
