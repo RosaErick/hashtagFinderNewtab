77d1aa9631268882c3fa349d7cf6e004a41b7e1a
Samir Santos <santossamir@hotmail.com>
2022-02-18 16:41:36 -0300

    Adicionando os buttons na navBar, as fotos perfil,descriçao e links nos icons dos cards de cada uma. Faltando os da mensagem.

--- src/pages/about/AppAbout.jsx
+++ src/pages/about/AppAbout.jsx
@@ -3,15 +3,17 @@ import Ilustration from "../../assets/img/aboutIlustration.svg";
 import IconGitHub from "../../assets/img/iconGithub.svg";
 import IconLinkedin from "../../assets/img/iconAwesomeLinkedin.svg";
 import IconMessage from "../../assets/img/iconEnvelope.svg";
+import DanielDenardi from "../../assets/img/danielDenardi.jpeg";
 import ErickRosa from "../../assets/img/erickRosa.jpeg";
+import LuanDunas from "../../assets/img/luanDunas.jpeg";
 import SamirSantos from "../../assets/img/samirSantos.jpeg";
 import Footer from "../../components/Footer";
 import Navbar from "../../components/Navbar";
 import "../../css/AppStyleAbout.css";
 
 // navbar icons
-import logIcon from "../../assets/img/icon-home.svg";
-import logoutIcon from "../../assets/img/icon-power-off.svg"
+import iconInfo from "../../assets/img/iconInfoCircle.svg";
+import iconUser from "../../assets/img/iconUserAlt.svg"
 
 
 
@@ -20,7 +22,9 @@ export default function App () {
   
   return(
       <div className="divContainer">
-        <Navbar buttons={[{"title": "Home", "icon": logIcon, "route": "/","backgroundColor": "#72EFDB", "textColor": "#0A1744"}, {"title": "Sair", "icon": logoutIcon, "route": "/sair","backgroundColor": "#0A1744", "textColor": "#FFF"}]}/>
+        <Navbar buttons={[{"title": "Sobre", "icon": iconInfo, "route": "/about","backgroundColor": "#72EFDB", "textColor": "#0A1744"},
+                          {"title": "Login", "icon": iconUser, "route": "/login","backgroundColor": "#0A1744", "textColor": "#FFF"}
+                        ]}/>
         <body>
           <div className="divAboutProject">
             <div className="descriptionAboutProject">
@@ -47,17 +51,17 @@ export default function App () {
             <h2>Quem somos</h2>
             <div className="boxAboutWe">
               <div className="profile">
-                <img className="imgProfile" src="#"/>
+                <img className="imgProfile" src={DanielDenardi}/>
                 <h4>Daniel Denardi</h4>
                 <p>
                   M.e em Ciências da Linguagem, Eng. Elétrico e Psicólogo,
-                  aprimorando habilidades técnicas em Desenvolvimento Web Full Stack,
-                  com foco em React. 
+                  aprimorando Desenvolvimento Web Full Stack,
+                  foco em React. 
                 </p>
                 <div className="ourIcons">
-                  <img src={IconGitHub}/>
-                  <img src={IconMessage}/>
-                  <img src={IconLinkedin}/>
+                  <a href="https://github.com/dandenardi"><img src={IconGitHub}/></a>
+                  <a href=""><img src={IconMessage}/></a>
+                  <a href="https://www.linkedin.com/in/danieldenardi/"><img src={IconLinkedin}/></a>
                 </div>
               </div>
               <div className="profile">
@@ -65,38 +69,38 @@ export default function App () {
                 <h4>Erick Rosa</h4>
                 <p>
                   Esp. em Tradução de Inglês, Ldo. em História, aprimorando
-                  habilidades técnicas em Desenvolvimento Web, com foco em Front End.
+                  Desenvolvimento Web, foco em Front End.
                 </p>
                 <div className="ourIcons">
-                  <img src={IconGitHub}/>
-                  <img src={IconMessage}/>
-                  <img src={IconLinkedin}/>
+                  <a href="https://github.com/RosaErick"><img src={IconGitHub}/></a>
+                  <a href=""><img src={IconMessage}/></a>
+                  <a href="https://www.linkedin.com/in/erick-rosa-dev//"><img src={IconLinkedin}/></a>
                 </div>
               </div>
               <div className="profile">
-                <img className="imgProfile" src="#"/>
+                <img className="imgProfile" src={LuanDunas}/>
                 <h4>Luan Santos</h4>
                 <p>
-                  Tec. em Multimídia e Design Gráfico, Desenvolvedor Júnior pioneiro
-                  com os Bots no Discord, e aprimorando habilidades técnicas em Desenvolvimento Web Full Stack.
+                  Tec. em Multimídia, Dev. Jr. pioneiro com Bots no Discord, aprimorando
+                  Desenvolvimento Web Full Stack.
                 </p>
                 <div className="ourIcons">
-                  <img src={IconGitHub}/>
-                  <img src={IconMessage}/>
-                  <img src={IconLinkedin}/>
+                  <a href="https://github.com/luandunas"><img src={IconGitHub}/></a>
+                  <a href=""><img src={IconMessage}/></a>
+                  <a href="https://www.linkedin.com/in/luandunas/"><img src={IconLinkedin}/></a>
                 </div>
               </div>
               <div className="profile">
                 <img className="imgProfile" src={SamirSantos}/>
                 <h4>Samir Santos</h4>
                 <p>
-                   M.e em Ciências da Educação, Ldo. em Ciências Biológicas,
-                   aprimorando habilidades técnicas em Desenvolvimento Web Full Stack.
+                   M.e em Ciências da Educação, Ldo. em Biologia,
+                   aprimorando Desenvolvimento Web Full Stack.
                 </p>
                 <div className="ourIcons">
-                  <img src={IconGitHub}/>
-                  <img src={IconMessage}/>
-                  <img src={IconLinkedin}/>
+                  <a href="https://github.com/santossamir"><img src={IconGitHub}/></a>
+                  <a href=""><img src={IconMessage}/></a>
+                  <a href="https://www.linkedin.com/in/samir-santos-88191519b/"><img src={IconLinkedin}/></a>
                 </div>
               </div>
             </div>
