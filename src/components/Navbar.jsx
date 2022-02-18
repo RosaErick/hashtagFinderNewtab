e3cf8a7f7db776d1607be888e8fa14f1d1bbbd4c
Erick Rosa <erickpmotta@gmail.com>
2022-02-17 21:45:17 -0300

    adicionando react link pro login e adicionando css no home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import '../css/nav-footer.css';
 import infoIcon from '../assets/img/icon-info-circle.svg';
 import logIcon from '../assets/img/icon-user-alt.svg'
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
     <>
       <nav class="nav-header">
+   
         <div class="nav-title">
           <h2>
             hashtag<span>finder</span>
@@ -15,8 +17,9 @@ export default function Navbar() {
 
         <div class="nav-menu">
           <button> <img src={infoIcon} alt="info-icon" /> Sobre</button>
-          <button> <img src={logIcon} alt="icon-user" /> Login</button>
-        </div>
+          <button> <img src={logIcon} alt="icon-user"/><Link to="/login">Login</Link></button>
+          </div>
+      
       </nav>
     </>
   );
