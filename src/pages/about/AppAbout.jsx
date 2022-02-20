e168858521213ac340b5c5817b6ff9bf4136fd57
Samir Santos <santossamir@hotmail.com>
2022-02-20 17:16:00 -0300

    Adicionando uma API para os cards.

--- src/pages/about/AppAbout.jsx
+++ src/pages/about/AppAbout.jsx
@@ -1,12 +1,10 @@
 import React from "react";
+import {useState, useEffect} from "react";
+import axios from "axios";
 import Ilustration from "../../assets/img/aboutIlustration.svg";
 import IconGitHub from "../../assets/img/iconGithub.svg";
 import IconLinkedin from "../../assets/img/iconAwesomeLinkedin.svg";
 import IconMessage from "../../assets/img/iconEnvelope.svg";
-import DanielDenardi from "../../assets/img/danielDenardi.jpeg";
-import ErickRosa from "../../assets/img/erickRosa.jpeg";
-import LuanDunas from "../../assets/img/luanDunas.jpeg";
-import SamirSantos from "../../assets/img/samirSantos.jpeg";
 import Footer from "../../components/Footer";
 import Navbar from "../../components/Navbar";
 import "../../css/AppStyleAbout.css";
@@ -15,10 +13,16 @@ import "../../css/AppStyleAbout.css";
 import iconInfo from "../../assets/img/iconInfoCircle.svg";
 import iconUser from "../../assets/img/iconUserAlt.svg"
 
-
-
-
 export default function App () {
+
+  const [itens, setItens] = useState([]);
+
+  useEffect(()=>{
+		axios.get("https://apiprojectteam.samirteixeira.repl.co/")
+			.then((resposta)=>{
+				setItens(resposta.data)
+		    })
+	}, []);
   
   return(
       <div className="divContainer">
@@ -49,59 +53,20 @@ export default function App () {
           <div className="divWhoWeAre">
             <h2>Quem somos</h2>
             <div className="boxAboutWe">
-              <div className="profile">
-                <img className="imgProfile" src={DanielDenardi}/>
-                <h4>Daniel Denardi</h4>
-                <p>
-                  M.e em Ciências da Linguagem, Eng. Elétrico e Psicólogo,
-                  aprimorando Desenvolvimento Web Full Stack,
-                  foco em React. 
-                </p>
-                <div className="ourIcons">
-                  <a href="https://github.com/dandenardi"><img src={IconGitHub}/></a>
-                  <a href=""><img src={IconMessage}/></a>
-                  <a href="https://www.linkedin.com/in/danieldenardi/"><img src={IconLinkedin}/></a>
+              {itens.map(item =>(
+                <div key={item.id} className="profile">
+                  <img className="imgProfile" src={item.img}/>
+                  <h4>{item.name}</h4>
+                  <p>
+                    {item.description} 
+                  </p>
+                  <div className="ourIcons">
+                    <a href={item.linkGitHub}><img src={IconGitHub}/></a>
+                    <a href={item.linkMessage}><img src={IconMessage}/></a>
+                    <a href={item.linkLinkedin}><img src={IconLinkedin}/></a>
+                  </div>
                 </div>
-              </div>
-              <div className="profile">
-                <img className="imgProfile" src={ErickRosa}/>
-                <h4>Erick Rosa</h4>
-                <p>
-                  Esp. em Tradução de Inglês, Ldo. em História, aprimorando
-                  Desenvolvimento Web, foco em Front End.
-                </p>
-                <div className="ourIcons">
-                  <a href="https://github.com/RosaErick"><img src={IconGitHub}/></a>
-                  <a href=""><img src={IconMessage}/></a>
-                  <a href="https://www.linkedin.com/in/erick-rosa-dev//"><img src={IconLinkedin}/></a>
-                </div>
-              </div>
-              <div className="profile">
-                <img className="imgProfile" src={LuanDunas}/>
-                <h4>Luan Santos</h4>
-                <p>
-                  Tec. em Multimídia, Dev. Jr. pioneiro com Bots no Discord, aprimorando
-                  Desenvolvimento Web Full Stack.
-                </p>
-                <div className="ourIcons">
-                  <a href="https://github.com/luandunas"><img src={IconGitHub}/></a>
-                  <a href=""><img src={IconMessage}/></a>
-                  <a href="https://www.linkedin.com/in/luandunas/"><img src={IconLinkedin}/></a>
-                </div>
-              </div>
-              <div className="profile">
-                <img className="imgProfile" src={SamirSantos}/>
-                <h4>Samir Santos</h4>
-                <p>
-                   M.e em Ciências da Educação, Ldo. em Biologia,
-                   aprimorando Desenvolvimento Web Full Stack.
-                </p>
-                <div className="ourIcons">
-                  <a href="https://github.com/santossamir"><img src={IconGitHub}/></a>
-                  <a href=""><img src={IconMessage}/></a>
-                  <a href="https://www.linkedin.com/in/samir-santos-88191519b/"><img src={IconLinkedin}/></a>
-                </div>
-              </div>
+              ))}
             </div>
           </div>
         <Footer />
