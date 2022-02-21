5c0f0d72991ab2e1847c0bea640aaff52a122724
luandunas <luandunaas@gmail.com>
2022-02-21 16:16:59 -0300

    Alterado comportamento do formulario Login para testar o redirecionamento do netlify

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -4,56 +4,68 @@ import iconHome from "../../assets/img/icon-home.svg";
 
 import '../../css/login.css';
 
-function Login() {
-    return (
-      <>
-        <Navbar buttons={[
-            {"title": "Home", 
-              "icon": iconHome, 
-              "route": "/",
-              "backgroundColor": "#72EFDB", 
-            "textColor": "#0A1744",
-             "margin":"0 0 0 80px",
-          },
-                
-        ]}/>
-        
-        <main className="mainLogin">
-            
-          
-          <div className="formContainer">
-            
-            <form className="loginForm">
-                <h2>Login</h2>
-                <div className="formInput">
-                  <label htmlFor="username">Usuário</label>
-                  <input type="text" name="username" id="username" placeholder=" " autoComplete="off" className="formInput" required />
-                </div>
-                <div className="formInput">
-                  <label htmlFor="password">Senha</label>
-                  <input type="password" name="password" id="password" placeholder=" " autoComplete="off" className="formInput" required />
-                  
-                </div>
-              
-                <div className="navBtn">
-                  <div className="navMenu">
-                      <button className="submitBtn">
-                        {" "}
-                        ACESSAR
-                      </button>
-                  </div>
-                </div>
-              
-            </form>
-          </div>
 
-        </main>
-        <Footer />
-      </>
-      
-      
-    );
+//Testing netlify routes
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+  let navigate = useNavigate();
+  function handleSubmit(e){
+    e.preventDefault();
+    e.stopPropagation();
+    navigate("/list");
   }
+
   
-  export default Login;
-  
+  return (
+    <>
+      <Navbar buttons={[
+        {
+          "title": "Home",
+          "icon": iconHome,
+          "route": "/",
+          "backgroundColor": "#72EFDB",
+          "textColor": "#0A1744",
+          "margin": "0 0 0 80px",
+        },
+
+      ]} />
+
+      <main className="mainLogin">
+
+
+        <div className="formContainer">
+
+          <form className="loginForm" onSubmit={handleSubmit}>
+            <h2>Login</h2>
+            <div className="formInput">
+              <label htmlFor="username">Usuário</label>
+              <input type="text" name="username" id="username" placeholder=" " autoComplete="off" className="formInput" required />
+            </div>
+            <div className="formInput">
+              <label htmlFor="password">Senha</label>
+              <input type="password" name="password" id="password" placeholder=" " autoComplete="off" className="formInput" required />
+
+            </div>
+
+            <div className="navBtn">
+              <div className="navMenu">
+                <button className="submitBtn">
+                  {" "}
+                  ACESSAR
+                </button>
+              </div>
+            </div>
+
+          </form>
+        </div>
+
+      </main>
+      <Footer />
+    </>
+
+
+  );
+}
+
+export default Login;
