8e37dc526c77c0148a2e9657fc81d1a433901352
luandunas <luandunaas@gmail.com>
2022-03-02 16:24:37 -0300

    Removido problema na variavel 'year' que estava removendo os primeiros numeros.

--- src/api/AirtablePOST.jsx
+++ src/api/AirtablePOST.jsx
@@ -7,7 +7,7 @@ const postData = async (postText) => {
 
   const day = `0${date.getDate()}`.slice(-2);
   const month = `0${date.getMonth()+1}`.slice(-2);
-  const year = `0${date.getFullYear()}`.slice(-2);
+  const year = date.getFullYear();
 
   const requestOptions = {
     method: "POST",
