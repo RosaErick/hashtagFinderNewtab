3cc0eb7a3eb82c83b4aa137829c6952c6f44adb7
luandunas <luandunaas@gmail.com>
2022-03-04 10:03:27 -0300

    Alterado component para fazer o POST com horario formatado de forma correta, seguindo o padrão XX:XX. Exemplo, 09:56 invés de 9:56 ou 21:01 invés de 21:1

--- src/api/AirtablePOST.jsx
+++ src/api/AirtablePOST.jsx
@@ -2,8 +2,8 @@ const APIPOST = "https://api.airtable.com/v0/app6wQWfM6eJngkD4/Buscas";
 
 const postData = async (postText) => {
   const date = new Date();
-  const hour = date.getHours();
-  const min = date.getMinutes();
+  const hour = `0${date.getHours()}`.slice(-2);
+  const min = `0${date.getMinutes()}`.slice(-2);
 
   const day = `0${date.getDate()}`.slice(-2);
   const month = `0${date.getMonth()+1}`.slice(-2);
